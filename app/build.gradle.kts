80ba53c6f1e79663b4f39d59df693b792eed5a46
serzh test <Artur1993xxx@yandex.ru>
2025-04-07 20:31:28 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@
  *
  * This generated file contains a sample Java application project to get you started.
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/8.10/userguide/building_java_projects.html in the Gradle documentation.
- * This project uses @Incubating APIs which are subject to change.
  */
 
 plugins {
@@ -17,18 +16,13 @@ repositories {
 }
 
 dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     // This dependency is used by the application.
     implementation(libs.guava)
-}
-
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit Jupiter test framework
-            useJUnitJupiter("5.10.3")
-        }
-    }
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -41,4 +35,9 @@ java {
 application {
     // Define the main class for the application.
     mainClass = "org.example.App"
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
 }
